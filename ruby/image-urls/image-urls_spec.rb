d71a080eb2542bd64b8c41a31ed8d21331a28e63
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-04 22:21:36 -0700

    add image_url methods and basic tests for instagram

--- ruby/image-urls/image-urls_spec.rb
+++ ruby/image-urls/image-urls_spec.rb
@@ -8,4 +8,59 @@ describe Post do
     p = Post.new(:attachment_url => instagram_url)
     expect(p.attachment_url).to eq instagram_url
   end
+
+  context "posts with pictures" do
+    instagram_url       = "http://instagram.com/p/iuuZxfOjEj/"
+    twitpic_url         = "http://twitpic.com/1e10q"
+    pic_twitter_url     = "https://pbs.twimg.com/media/BdLBbkpCcAAspZn.jpg"
+    pic_twitter_png_url = "https://pbs.twimg.com/media/BdLBbkpCcAAspZn.png"
+    yfrog_url           = "http://yfrog.com/oefi8whj"
+
+    let!(:nil_pic)     { Post.new(:attachment_url => nil) }
+    let!(:invalid_pic) { Post.new(:attachment_url => "google.com") }
+    let!(:insta)       { Post.new(:attachment_url => instagram_url) }
+    let!(:twitpic)     { Post.new(:attachment_url => twitpic_url) }
+    let!(:t_pic)       { Post.new(:attachment_url => pic_twitter_url) }
+    let!(:t_pic_png)   { Post.new(:attachment_url => pic_twitter_png_url) }
+    let!(:yfrog)       { Post.new(:attachment_url => yfrog_url) }
+    let!(:services)    { [
+      insta, twitpic, nil_pic, invalid_pic, t_pic, t_pic_png, yfrog
+    ] }
+
+    describe "with an Instagram photo" do
+      medium_size_url = "http://instagr.am/p/iuuZxfOjEj/media/?size=m"
+      thumb_url       = "http://instagr.am/p/iuuZxfOjEj/media/?size=t"
+
+      it "should have an instagram photo" do
+        expect(insta.has_instagram?).to be_true
+      end
+
+      it "should return the instagram full url" do
+        expect(insta.instagram_url).to eq medium_size_url
+      end
+
+      it "should return the instagram thumb url" do
+        expect(insta.instagram_thumb_url).to eq thumb_url
+      end
+
+      it "should have an instagram url for image_url" do
+        expect(insta.image_url).to eq insta.instagram_url
+      end
+
+      it "should have an instagram thumb url for thumb_url" do
+        expect(insta.image_thumb_url).to eq insta.instagram_thumb_url
+      end
+    end
+
+    describe "without an instagram photo" do
+      it "shouldn't have any instagram jazz" do
+        services.each do |tweet|
+          next if tweet == insta
+          expect(tweet.has_instagram?).to be_false
+          expect(tweet.instagram_url).to be_nil
+          expect(tweet.instagram_thumb_url).to be_nil
+        end
+      end
+    end
+  end
 end
--- ruby/image-urls/original.rb
+++ ruby/image-urls/original.rb
@@ -5,4 +5,89 @@ class Post
   def initialize(attributes={})
     @attachment_url = attributes[:attachment_url]
   end
+
+  INSTAGRAM_ID  = %r{instagram.com/p/([\w-]+)}
+
+  def image_url
+    @image_url ||= get_image_url_from_services || nil
+  end
+
+  def image_thumb_url
+    @image_thumb_url ||= get_image_thumb_url_from_services
+  end
+
+  def get_image_url_from_services
+    instagram_url ||
+    twitpic_url ||
+    twitter_pic_url ||
+    yfrog_url
+  end
+
+  def get_image_thumb_url_from_services
+    instagram_thumb_url ||
+    twitpic_thumb_url ||
+    twitter_pic_thumb_url ||
+    yfrog_thumb_url
+  end
+
+  def has_yfrog?
+    yfrog_url
+  end
+
+  def has_twitter_pic?
+    twitter_pic_url
+  end
+
+  def has_twitpic?
+    twitpic_url
+  end
+
+  def has_instagram?
+    instagram_url
+  end
+
+  def twitter_pic_url
+    "#{attachment_url}:medium" if !attachment_url.to_s.scan(TWITTER_PIC).empty?
+  end
+
+  def twitter_pic_thumb_url
+    "#{attachment_url}:thumb" if twitter_pic_url
+  end
+
+  def instagram_url
+    attachment_url.to_s.scan(INSTAGRAM_ID).flatten.map do |id|
+      "http://instagr.am/p/#{id}/media/?size=m"
+    end.first
+  end
+
+  def instagram_thumb_url
+    attachment_url.to_s.scan(INSTAGRAM_ID).flatten.map do |id|
+      "http://instagr.am/p/#{id}/media/?size=t"
+    end.first
+  end
+
+  def twitpic_url
+    attachment_url.to_s.scan(TWITPIC_ID).flatten.map do |id|
+      "http://twitpic.com/show/full/#{id}"
+    end.first
+  end
+
+  def twitpic_thumb_url
+    attachment_url.to_s.scan(TWITPIC_ID).flatten.map do |id|
+      "http://twitpic.com/show/mini/#{id}"
+    end.first
+  end
+
+  def yfrog_url
+    attachment_url.to_s.scan(YFROG_ID).flatten.map do |id|
+      "http://yfrog.com/#{id}:iphone"
+    end.first
+  end
+
+  def yfrog_thumb_url
+    attachment_url.to_s.scan(YFROG_ID).flatten.map do |id|
+      "http://yfrog.com/#{id}:small"
+    end.first
+  end
+
 end
