6b077e3fd6b35d606fb9e297a58a4634ab87d8aa
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-05 00:08:34 -0700

    make #present_image_service_data more explicit by extracting into a couple more methods

--- ruby/image-urls/refactor_v1.rb
+++ ruby/image-urls/refactor_v1.rb
@@ -18,58 +18,50 @@ class Post
     present_image_service_data.first
   end
 
-  def image_services
-    [
-      :instagram,
-      :twitpic,
-      :yfrog,
-      :twitter_pic
-    ]
-  end
-
   private
 
   def image_url_of_size(size)
     service, url = present_image_service_data
-    send(service)[size].call url if service
+    return image_services[service][size].call url if service
   end
 
   def present_image_service_data
-    @present_image_service_data ||= image_services.map do |service|
-      url = attachment_url.to_s.scan(send(service)[:regex]).flatten.first
-      [service, url] if url
+    @present_image_service_data ||= image_services.keys.map do |service|
+      id = image_id_for(service)
+      return [service, id] if id
     end.compact.flatten
   end
 
-  def instagram
-    {
-      :regex     => %r{instagram.com/p/([\w-]+)},
-      :full  => ->(id) { "http://instagr.am/p/#{id}/media/?size=m" },
-      :thumb => ->(id) { "http://instagr.am/p/#{id}/media/?size=t" }
-    }
+  def image_id_for(service)
+    attachment_url.to_s.scan(regex_for(service)).flatten.first
   end
 
-  def yfrog
-    {
-      :regex     => %r{yfrog.[^/]+/([a-zA-Z0-9]+[jptg])},
-      :full  => ->(id) { "http://yfrog.com/#{id}:iphone" },
-      :thumb => ->(id) { "http://yfrog.com/#{id}:small" }
-    }
+  def regex_for(service)
+    image_services[service][:regex]
   end
 
-  def twitpic
+  def image_services
     {
-      :regex     => %r{twitpic.com/([a-zA-Z0-9]+)},
-      :full  => ->(id) { "http://twitpic.com/show/full/#{id}" },
-      :thumb => ->(id) { "http://twitpic.com/show/mini/#{id}" }
-    }
-  end
-
-  def twitter_pic
-    {
-      :regex     => %r{pbs.twimg.com/media/([\w]+)},
-      :full  => ->(id) { "#{attachment_url}:medium" if id },
-      :thumb => ->(id) { "#{attachment_url}:thumb" if id}
+      :instagram => {
+        :regex => %r{instagram.com/p/([\w-]+)},
+        :full  => ->(id) { "http://instagr.am/p/#{id}/media/?size=m" },
+        :thumb => ->(id) { "http://instagr.am/p/#{id}/media/?size=t" }
+      },
+      :yfrog => {
+        :regex => %r{yfrog.[^/]+/([a-zA-Z0-9]+[jptg])},
+        :full  => ->(id) { "http://yfrog.com/#{id}:iphone" },
+        :thumb => ->(id) { "http://yfrog.com/#{id}:small" }
+      },
+      :twitpic => {
+        :regex => %r{twitpic.com/([a-zA-Z0-9]+)},
+        :full  => ->(id) { "http://twitpic.com/show/full/#{id}" },
+        :thumb => ->(id) { "http://twitpic.com/show/mini/#{id}" }
+      },
+      :twitter_pic => {
+        :regex => %r{pbs.twimg.com/media/([\w]+)},
+        :full  => ->(id) { "#{attachment_url}:medium" if id },
+        :thumb => ->(id) { "#{attachment_url}:thumb" if id }
+      }
     }
   end
 
